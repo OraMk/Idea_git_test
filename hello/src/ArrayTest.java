d0cd3ef000040bdf4e4e80c784b78eeef8f04a1d
Oramk <1710025170@qq.com>
2024-07-08 20:19:11 +0800

    添加comm

--- hello/src/ArrayTest.java
+++ hello/src/ArrayTest.java
@@ -16,6 +16,7 @@ class MyTools {
 
     public void bubble(int[] arr) {
         int temp;
+        int comm = 100;
         for (int i = 0; i < arr.length - 1; i++) {
 
             for (int j = 0; j < arr.length - 1 - i; j++) {
