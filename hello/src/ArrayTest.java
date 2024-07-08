c876d2f4e4c1e3addd4d7a8795689353eac7617f
Oramk <1710025170@qq.com>
2024-07-08 21:20:26 +0800

    更改comm 为cao

--- hello/src/ArrayTest.java
+++ hello/src/ArrayTest.java
@@ -17,7 +17,7 @@ class MyTools {
     public void bubble(int[] arr) {
         int temp;
         int max;
-        int comm = 100;
+        int cao = 100;
         for (int i = 0; i < arr.length - 1; i++) {
 
             for (int j = 0; j < arr.length - 1 - i; j++) {
